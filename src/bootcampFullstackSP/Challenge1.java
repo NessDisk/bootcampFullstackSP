6ffc286b011bae04ef937b2cb8a1ca88703bc270
NessDisk <ness.cero1@gmail.com>
2022-11-11 20:23:04 -0500

    Change_name_Challenge1

--- src/bootcampFullstackSP/Challenge1.java
+++ src/bootcampFullstackSP/Challenge1.java
@@ -3,7 +3,7 @@ package bootcampFullstackSP;
 import java.util.*;
 
 
-public class Homework1 {
+public class Challenge1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
